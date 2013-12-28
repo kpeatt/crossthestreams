862c14480e70603a4a1d6f90f86bdfbde1ba953c
Tim Kim <timk@adobe.com>
2013-12-27 18:56:51 -0800

    Sample json file for kyle

--- server/index.js
+++ server/index.js
@@ -12,18 +12,37 @@ var twit = new twitter({
 function filter(arr, criteria) {
   return arr.filter(function(obj) {
     return Object.keys(criteria).every(function(c) {
-      return obj[c] == criteria[c];
+      return obj[c] >= criteria[c];
     });
   });
 }
 
-twit.get('/statuses/user_timeline.json', {count: 100, user_id:"timkim", screen_name:"timkim"}, function(data) {
+twit.get('/statuses/user_timeline.json', {count: 100, screen_name:"kpeatt", trim_user:true}, function(data) {
     //console.log(util.inspect(data));
-    console.log(filter(data,{'favorited': true}));
+    console.log(filter(data,{'favorite_count': 1}));
 });
 
 /*
 twit.verifyCredentials(function(data) {
         console.log(util.inspect(data));
     })
+*/
+
+// build up a score for tweets based upon 
+// existing entities.hashtags.length, favorite_count, retweet_count
+
+/*
+{
+    "answer":"javascript",
+    "text":"Tweet Button, Follow Button, and Web Intents $$$ANSWER$$$ now support SSL http:\/\/t.co\/9fbA0oYy ^TS",
+    "user":{
+        "created_at": "Mon Nov 29 21:18:15 +0000 2010",
+        "name":"Twitter API", 
+        "profile_image_url":"http:\/\/a2.twimg.com\/profile_images\/1438634086\/avatar_normal.png",
+        "screen_name":"twitterapi",
+    }
+    
+        
+    }
+}
 */
