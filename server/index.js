5b2b805226b0347e5a1400851ee01646ffb4d4a6
Tim Kim <timk@adobe.com>
2013-12-28 18:52:20 -0800

    Added filtering tweet functions for clue generation

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ var twit = new twitter({
     access_token_secret: secret.access_token_secret
 });
 
-function filter(arr, criteria) {
+function filterGTEquals(arr, criteria) {
   return arr.filter(function(obj) {
     return Object.keys(criteria).every(function(c) {
       return obj[c] >= criteria[c];
@@ -17,9 +17,62 @@ function filter(arr, criteria) {
   });
 }
 
-twit.get('/statuses/user_timeline.json', {count: 100, screen_name:"kpeatt", trim_user:true}, function(data) {
-    //console.log(util.inspect(data));
-    console.log(filter(data,{'favorite_count': 1}));
+function buildScore(arr){
+    for(var i=0; i<arr.length; i++){
+        if(!arr[i].score){
+            arr[i].score = 0;
+        }
+                        
+        if(arr[i].entities.hashtags.length > 0){
+            arr[i].score++;
+        }
+        
+        if(arr[i].favorite_count > 0){
+            arr[i].score++;
+        }
+        
+        if(arr[i].retweet_count > 0){
+            arr[i].score++;
+        }
+    }
+    return arr;
+}
+
+function generateClues(arr, length){
+    var newClues = [];
+    if(!length){
+        length = 10;
+    }
+    
+    for(var i=0; i<length; i++){
+        var theClue = {};
+        theClue.id = arr[i].id;
+        theClue.created_at = arr[i].created_at;
+        theClue.screen_name = arr[i].user.screen_name;
+        theClue.name = arr[i].user.name;
+        theClue.profile_background_image_url = arr[i].user.profile_background_image_url;
+        theClue.text = arr[i].text;
+        newClues[i] = theClue;
+    }
+    return newClues;
+}
+
+twit.get('/statuses/user_timeline.json', {count: 100, screen_name:"kpeatt"}, function(data) {
+    var scored = buildScore(data);
+    
+    var filtered = filterGTEquals(scored, { score : 1 });
+    
+    var sorted = filtered.sort(function(a, b){
+        if(a.score > b.score){
+            return -1;
+        }
+        if(a.score < b.score){
+            return 1;
+        }
+        return 0
+    });
+    console.log(generateClues(sorted));
+    //console.log(sorted);
 });
 
 /*
@@ -31,18 +84,3 @@ twit.verifyCredentials(function(data) {
 // build up a score for tweets based upon 
 // existing entities.hashtags.length, favorite_count, retweet_count
 
-/*
-{
-    "answer":"javascript",
-    "text":"Tweet Button, Follow Button, and Web Intents $$$ANSWER$$$ now support SSL http:\/\/t.co\/9fbA0oYy ^TS",
-    "user":{
-        "created_at": "Mon Nov 29 21:18:15 +0000 2010",
-        "name":"Twitter API", 
-        "profile_image_url":"http:\/\/a2.twimg.com\/profile_images\/1438634086\/avatar_normal.png",
-        "screen_name":"twitterapi",
-    }
-    
-        
-    }
-}
-*/
