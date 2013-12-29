c9405e767a37056744ace9505a0b961d013f6220
Kyle Peatt <kpeatt@gmail.com>
2013-12-28 18:16:25 -0800

    Look at the answers JSON and create form fields for input

--- www/js/controller.js
+++ www/js/controller.js
@@ -3,10 +3,38 @@ var crossApp = angular.module('crossApp', []);
 crossApp.controller(
     'tweetsCtrl', ['$scope', '$http',
         function($scope, $http) {
-            $scope.test = 'test';
-            $scope.tweets = 'fail';
+            window.MY_SCOPE = $scope; // For debugging scope
+            $scope.tweets = {};
+
+            $scope.answers = {};
+
             $http.get('../server/sample.json').success(function(data) {
                 $scope.tweets = data;
+
+                for (var i in $scope.tweets) {
+                    var tweet = $scope.tweets[i];
+                    var text = tweet['text'];
+                    var splitText = [];
+                    var answer = tweet['answer'];
+                    var splitAnswer = tweet['splitAnswer'];
+
+                    splitText = text.split('$$$ANSWER$$$');
+                    splitAnswer = answer.split('');
+
+                    tweet['text'] = splitText;
+                    tweet['splitAnswer'] = splitAnswer;
+
+                    chars = splitAnswer.map(function() {
+                        return ''
+                    })
+
+                    console.log(chars);
+
+                    $scope.answers[i] = {
+                        'user': chars,
+                        'correct': chars
+                    }
+                }
             });
         }
     ]
